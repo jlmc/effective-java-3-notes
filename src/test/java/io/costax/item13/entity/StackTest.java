4fa31cb0efa557b75089c2cab41c76ebc91894da
Joao Costa <costajlmpp@gmail.com>
2019-05-25 23:36:48 +0100

    migration to junit 5

--- src/test/java/io/costax/item13/entity/StackTest.java
+++ src/test/java/io/costax/item13/entity/StackTest.java
@@ -2,24 +2,28 @@ package io.costax.item13.entity;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
 
 public class StackTest {
 
     @Test
     public void testSkackCloneMethod() {
 
-        final Stack x = new Stack();
-        final PhoneNumber pn = PhoneNumber.from((short) 1, (short) 2, (short) 3);
-        x.push(pn);
+        final Stack stack = new Stack();
 
-        final Stack clone = x.clone();
+        final PhoneNumber phoneNumber = PhoneNumber.from((short) 1, (short) 2, (short) 3);
+        stack.push(phoneNumber);
 
-        assertTrue(clone != x);
-        assertTrue(x.getClass() == clone.getClass());
+        final Stack clone = stack.clone();
+
+        assertNotSame(clone, stack);
+        assertSame(stack.getClass(), clone.getClass());
         //assertTrue(x.equals(clone)); // stack do not implements equals methods
         final Object pop = clone.pop();
-        assertTrue(pop != pn);
+
+        // prove that there are not deep copy o the references objects
+        assertSame(pop, phoneNumber);
 
     }
 }
